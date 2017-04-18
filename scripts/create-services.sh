708070b2cfc23e93e139ef2529f58146cce80f74
Ankita Kulkarni <ankitadk@ca.ibm.com>
2017-04-18 15:38:00 -0400

    updated scripts for automation

--- scripts/create-services.sh
+++ scripts/create-services.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
 
 echo 'Make sure you are in the right space and trying logging in using cf login'
-echo  'usage: bash create-services.sh <app name to bind the service to>'
 
-app=$1
-
-if [ "$app" ]
-then
-    cf create-service compose-for-elasticsearch Standard icon-elasticsearch
-    cf create-service compose-for-postgresql Standard icon-postgresql
-    cf create-service compose-for-rabbitmq Standard icon-rabbitmq
-
-    # cf bs $app icon-elasticsearch
-    # cf bs $app icon-postgresql
-    # cf bs $app icon-rabbitmq
-else
-    echo 'Unable to create/bind the service: check your app name'
-fi
-
+cf create-service compose-for-elasticsearch Standard icon-elasticsearch
+cf create-service compose-for-postgresql Standard icon-postgresql
+cf create-service compose-for-rabbitmq Standard icon-rabbitmq
--- scripts/create_user_provided_service.sh
+++ scripts/create_user_provided_service.sh
@@ -6,12 +6,16 @@ if [ $# -eq 0 ]
     echo 'Make sure you are in the right space and trying logging in using cf login'
   else
     json_file=$1
-    app=$2
 
-    if [ "$json_file" -a "$app" ]
+    if [ "$json_file" ]
     then
-        cf uups env_setup -p $json_file
-        cf bs $app env_setup
+        ups=$(cf service env_setup --guid)
+        if [ ! -z $UPS ] 
+        then
+            cf cups env_setup -p $json_file
+        else
+            cf uups env_setup -p $json_file
+        fi
     else
         echo 'Unable to create/bind the service: invalid path or file does not exist or check your app name'
     fi
